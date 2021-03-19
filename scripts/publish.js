c39f61918fd437c92b450524cf86d6a10eb587aa
Shr1ftyy <sajid@Sajids-MacBook-Air.local>
2021-03-19 20:31:12 +1030

    almost done with IPFS

--- scripts/publish.js
+++ scripts/publish.js
@@ -5,14 +5,19 @@ const MIN = 1; // The user must insert at least 1 character, otherwise it will n
 async function publishToIPFS() { 
   const msg = document.getElementById("msg").value;
   const release_date = document.getElementById("release_date").value;
+  const release_time = document.getElementById("release_time").value;
+  if (release_date.toString().length < 1 || release_time.toString().length < 1){
+    document.getElementById("out").innerHTML = "You must fill out all fields" 
+		ipfs.stop()
+    return
+  }
   if (msg.length < MAX && msg.length > MIN){
     document.getElementById("out").innerHTML = "Deploying...";
     const ipfs = await Ipfs.create();
     const { cid } = await ipfs.add(msg);
     console.log(cid);
     console.log(release_date);
-
-    var database = firebase.database();
+    writeCapsule(msg, release_date, release_time)
 
     document.getElementById("out").innerHTML = "Time capsule deployed to: " + cid;
     console.log(Object.keys(ipfs));
@@ -23,10 +28,10 @@ async function publishToIPFS() {
 }
 
 function writeCapsule(msg, date, time) { 
-  firebase.database().ref('users/' + userId).set({
+  var db = firebase.database();
+  db.ref('private_capsules/' + Date.now().toString()).set({
     msg: msg,
     date: date,
     time : time,
   });
-  }
 }
